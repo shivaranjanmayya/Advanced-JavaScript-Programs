7dd6dc4ae2c652afd9d08f7137dd4f2cac9bf55d
shivaranjanmayya <nsmayya@gmail.com>
2021-11-23 08:50:09 +0530

    Modified the code

--- project/form/elements.js
+++ project/form/elements.js
@@ -1,8 +1,9 @@
 var div = document.createElement('div');
 div.id = "result";
-document.body.appendChild(div);
 var div1 = document.createElement('div');
+document.body.appendChild(div);
 document.body.appendChild(div1);
+
 var p = document.createElement('P');
 p.id = "para1";
 var h1 = document.createElement('H1');
@@ -11,6 +12,8 @@ h1.innerText = "First Paragraph";
 document.body.appendChild(h1);
 p.innerHTML = "This is my First Paragraph";
 document.body.appendChild(p);
+div1.appendChild(p);
+div1.appendChild(h1);
 
 var p = document.createElement('P');
 p.id = "para2";
@@ -20,3 +23,7 @@ h1.innerText = "Second Paragraph";
 document.body.appendChild(h1);
 p.innerHTML = "This is my Second Paragraph";
 document.body.appendChild(p);
+div1.appendChild(p);
+div1.appendChild(h1);
+
+div.appendChild(div1);
